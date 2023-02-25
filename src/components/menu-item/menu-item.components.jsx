85ce7c4c0c7f6350915c14c6333e3fe00bb6d934
zakaria <zguellouch@gmail.com>
2023-02-25 18:39:30 +0100

    create ecom store

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -3,12 +3,12 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({title, key, image, size}) => ( //{title} = props.title
     <div className={`${size} menu-item`}>
-        <div className="background-image" style={{backgroundImage: `URL(${image})`}}>
+        <div className="background-image" style={{backgroundImage: `URL(${image})`}}></div>
             <div className="content">
                 <h1 className="title" key={`${key}`} >{title.toUpperCase()}</h1>
                 <span className="subtitle">Shop now</span>
             </div>
-        </div>
+        
     </div>
 )
 
